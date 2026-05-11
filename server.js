d2aaa9d26aecee4a0a08809bbd942eaff23bdc69
ds9423051-boop <ds9423051@stud.juntendo.ac.jp>
2026-05-12 02:48:14 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/download', (req, res) => {
+    res.redirect('https://YOUR-SITE-B.vercel.app/');
+});
+
+module.exports = app;
