c269cb0071cb3c5fd9a8e16122a1c15d72245f21
ds9423051-boop <ds9423051@stud.juntendo.ac.jp>
2026-05-12 03:07:57 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/download', (req, res) => {
-    res.redirect('https://YOUR-SITE-B.vercel.app/');
+
+  res.redirect('https://coral-theta.vercel.app/');
+
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('server started');
+});
 module.exports = app;
